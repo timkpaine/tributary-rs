cb15f0043ed0ca4e9e6f3059bc541f313640cf48
Tim Paine <t.paine154@gmail.com>
2023-04-03 17:32:40 -0400

    wip

--- src/main.rs
+++ src/main.rs
@@ -7,11 +7,19 @@ fn main() {
   let n1 = Node::from(|()| 5);
   let n2 = Node::from(|()| 6);
   let n4 = Node::from(|x: u32| -> () { });
+
   let n3 = n1 + n2;
   let print = Print("Out:", n3);
-
+  
   // TODO
   // let out = print >> n4
   // run(out, true, None, None);
   run(print, true, None, None);
+
+
+  // some random tests
+  let n4 = Node::from(|x: u32| -> u32 { return x; });
+  let n4 = Node::from(|(x, y)| -> (u32, u32) {return (x, y);} );
+  let n4 = Node::from(|(x)| -> (u32, u32) {return (x, x);} );
+  let n4 = Node::from(|()| -> (u32, u32) {return (4, 5);} );
 }
